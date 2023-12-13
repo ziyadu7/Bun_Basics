193324ef31341c897a456a2af6f4984c77a33688
ziyadu7 <muhdziyadu@gmail.com>
2023-12-13 14:09:34 +0530

    practiced test.ts

---
+++ index.test.ts
@@ -0,0 +1,13 @@
+import {describe, expect, test, beforeAll} from 'bun:test'
+
+beforeAll(()=>{
+    // Setup tests
+})
+
+// ACTUALL TEST FUNCTION
+
+describe('math',()=>{
+    test('addition',()=>{
+        expect(2+2).toBe(4)
+    })
+})
